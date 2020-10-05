9835bf5ec36d8d77326f51000e8286e3bd3c8a83
Prough123 <slipok1999@mail.ru>
2020-10-05 19:21:48 +0300

    Updates

---
+++ precache-manifest.6864f3711260edb8279eeb3ef8b8ff0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d15f9b10f8e09f05a08863e58f81f0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5ad8765aeaa1cec3dc5a",
+    "url": "/portfolio/static/css/main.5f098d83.chunk.css"
+  },
+  {
+    "revision": "6dbe76ba2a6c1a11dd68",
+    "url": "/portfolio/static/js/2.f2ac3847.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.f2ac3847.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ad8765aeaa1cec3dc5a",
+    "url": "/portfolio/static/js/main.9c30927a.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
